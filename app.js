046219d9955e17689ec2465afc04a5ec932cd2cf
David Dugger <davidmdj86@gmail.com>
2017-12-05 13:45:00 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var todoController = require("./controllers/todoController");
+
+// setup template engine
+app.set("view engine", "ejs");
+
+// static files
+app.use(express.static('./public'));
+
+// fire controllers
+todoController(app);
+
+// listen to port
+app.listen(3000);
+console.log("You are listening to port 3000");
